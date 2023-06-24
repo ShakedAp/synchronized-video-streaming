cb7fb85842e4fc7bc378b53770e41c5f9589af3b
ShakedAp <shaked.apter123@gmail.com>
2023-06-24 13:55:54 +0300

    Added web sockets
    Changed index.js to add WebSockets to the server
    Changed index.html to add WebSockets to the client
    Install ws (web sockets) module

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app);
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ server:server });
+
+wss.on('connection', function connection(ws) {
+  console.log('A new client Connected!');
+  ws.send('Welcome New Client!');
+});
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
-  });
+});
 
-app.listen(3000, function () {
-    console.log("Listening on port 3000!");
-  });
+server.listen(3000, () => console.log(`Lisening on port :3000`))
